2bfcd7861621140da8ab54333bff4c2a0d07c933
Kellen-Linse <kblinse06@gmail.com>
2022-01-10 17:45:07 -0800

    Fix issues

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,14 +1,13 @@
 'use strict';
 
-const supertest = require('supertest');
 const server = require('../app.js');
-
+const supertest = require('supertest');
 const request = supertest(server.app);
 
 describe('Testing my HTTP server', () => {
 
   it('should be able to respond to a POST request', async () => {
-    let response = await request.post('message?text=test&autor=test');
+    let response = await request.post('/message?text=test&author=test');
     
     expect(response.status).toEqual(200);
     expect(response.body[0].text).toEqual('test');
